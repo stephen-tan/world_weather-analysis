d8bf7c2d131a1e0d9dd122929e51794fc313e7be
Stephen Tan <82525229+stephen-tan@users.noreply.github.com>
2021-06-06 18:07:07 -0700

    Add files via upload

---
+++ Vacation_Itinerary/config.py
@@ -0,0 +1,5 @@
+weather_api_key = "a516769bbf338d45aaab9eaf1205d742"
+
+g_key = "AIzaSyAsl3xR9-2OOp6qqIjoeea374GZ1nL7tA4"
+
+gmap_key = "AIzaSyDM2k9E8P4IG9HJOPpkeTvGIhTyQNpyCdI"
---
+++ Vacation_Search/config.py
@@ -0,0 +1,3 @@
+weather_api_key = "a516769bbf338d45aaab9eaf1205d742"
+
+g_key = "AIzaSyAsl3xR9-2OOp6qqIjoeea374GZ1nL7tA4"
